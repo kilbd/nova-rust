06b82a07d1f3df2bdbbea72b73f1be6b6f7b0378
Drew Kilb <drew@kilb.com>
2021-09-29 22:23:08 -0400

    feat: Add handler for format on save
    This commit adds a call to `rustfmt` when a user saves. The method
    returns a Promise to have the issue checker await the reformat (so
    that highlighted ranges don't change). Unfortunately, Nova seems to
    pick up on the altered document *after* it has generated issues,
    and thus highlights are removed. Not the desired behavior.

--- src/rust-formatter.ts
+++ src/rust-formatter.ts
@@ -7,9 +7,30 @@ export class RustFormatter {
     })
   }
 
-  format() {
-    if (this.enabled) {
-      console.log('Format!')
-    }
+  format(editor: TextEditor): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (this.enabled) {
+        let fmtProcess = new Process('rustfmt', {
+          args: [editor.document.path as string],
+          shell: true,
+        })
+        fmtProcess.onStderr((err: string) => console.error(err))
+        fmtProcess.onDidExit((status: number) => {
+          if (status !== 0) {
+            let notification = new NotificationRequest('rustfmt.error')
+            notification.title = nova.localize('rustfmt Failed')
+            notification.body = nova.localize(
+              'If this error persists, please create a bug report/issue.'
+            )
+            // No user actions configured, so I'm ignoring the returned promise.
+            nova.notifications.add(notification)
+          }
+          resolve()
+        })
+        fmtProcess.start()
+      } else {
+        resolve()
+      }
+    })
   }
 }
