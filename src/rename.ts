c978bb66e31e89df9274705a962dd7277419c685
Drew Kilb <git@kilb.email>
2022-05-18 20:26:12 -0400

    refactor: Move input palette to own function
    To condense the rename function a bit, I moved creating the input
    palette to its own function, which returns a Promise. It either
    returns the desired new name, or errors (from user dismissing the
    palette) and causes rename to return early.

--- src/rename.ts
+++ src/rename.ts
@@ -1,4 +1,4 @@
-function rename(editor: TextEditor) {
+async function rename(editor: TextEditor) {
   const position = editor.selectedRange
   // Commenting out next few lines but keeping in case they're later useful.
   // Nova only recognizes something as a symbol where it is defined. Users will
@@ -10,15 +10,30 @@ function rename(editor: TextEditor) {
   editor.selectWordsContainingCursors()
   const oldName = editor.selectedText
   console.log(`rename ${oldName}`)
-  nova.workspace.showInputPalette(
-    `Rename: Enter a new name for '${oldName}' (ESC to cancel)`,
-    { value: oldName },
-    (input) => {
-      if (input) {
-        console.log(`new name: ${input}`)
+  let newName: string
+  try {
+    newName = await askForNewName(oldName)
+  } catch {
+    console.log('Rename aborted by user.')
+    return
+  }
+  console.log(newName)
+}
+
+function askForNewName(oldName: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    nova.workspace.showInputPalette(
+      `Rename: Enter a new name for '${oldName}' (ESC to cancel)`,
+      { value: oldName },
+      (input) => {
+        if (input) {
+          resolve(input)
+        } else {
+          reject()
+        }
       }
-    }
-  )
+    )
+  })
 }
 
 export { rename }
