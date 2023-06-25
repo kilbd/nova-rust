b9a40dfad67968ce1e0768239de6a8e04e1e1255
Drew Kilb <git@kilb.email>
2023-06-25 12:37:20 -0400

    feat: Add basic code folding queries
    Apparently the move to Tree Sitter removed the code folding
    provided for free to symbols. This commit implements folding
    queries for the most common items (structs, functions,
    blocks, etc.).
    
    Issues: #46

---
+++ Rust.novaextension/Queries/folds.scm
@@ -0,0 +1,43 @@
+((function_item 
+    body: (block
+        ("{") @start
+        ("}") @end ))
+(#set! role function))
+
+((struct_item
+    body: (field_declaration_list
+        ("{") @start
+        ("}") @end ))
+(#set! role type))
+
+((struct_item
+    body: (ordered_field_declaration_list
+        ("(") @start
+        (")") @end ))
+(#set! role type))
+
+((enum_item
+    body: (enum_variant_list
+        ("{") @start
+        ("}") @end ))
+(#set! role type))
+
+((impl_item
+    body: (declaration_list
+        ("{") @start
+        ("}") @end ))
+(#set! role type))
+
+((trait_item
+    body: (declaration_list
+        ("{") @start
+        ("}") @end ))
+(#set! role type))
+
+((block
+    ("{") @start
+    ("}") @end )
+(#set! role block))
+
+((line_comment) 
+(#set! role comment))
